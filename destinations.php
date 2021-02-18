7c3e4a7253bdfb7fd6957bb54d1b28a07fd40c86
carlospp24 <carlospantin@live.com.mx>
2021-02-18 20:25:25 +0100

    Code

---
+++ destinations.php
@@ -0,0 +1,48 @@
+<?php include 'header.html'?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+  <link rel="stylesheet" href="styling.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <title>Destinations</title>
+  </head>
+  <body>
+  <div class="container">
+            <div class="row">
+                 <div class="col-3">
+                 </div>
+                 <div class="col-6">
+                <h1 class="spain">Spain</h1>
+                 </div>
+                 <div class="col-3">
+                 </div>
+                </div>
+              </div>
+<div class="container">
+    <div class="row">
+        <div class="col-4">
+<h1>About Spain</h1>
+<p class="hometext">Spain, a beautiful country located on Europe's Iberian Peninsula is a perfect place for some well deserved vacations. Spain is one of Europe's most famous locations to spend some nice and tropical vacations. Spain has over 8,000 km of coastline and THOUSANDS of beaches. Spain also has over 300 days of sunshine throughout the year. </p>
+        </div>
+        <div class="col-8">
+<img src="http://localhost/Project/images/espana.jpg" alt="" style=" width: 800px;padding-top: 2rem; padding-left: 15rem; ">
+</div>
+    </div>
+
+</div>
+    
+  
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+  </body>
+
+  <footer>
+  <?php include 'footer.html';?>
+  </footer>
+</html>
